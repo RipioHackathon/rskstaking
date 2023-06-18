5e42d3decbaaa449bfaf7aab33b2faafa68fd521
haycarlitos <carlos.castilloperera@gmail.com>
2023-06-18 12:41:02 -0600

    config: add rsk chain and contact address

---
+++ const/Details.js
@@ -0,0 +1,1 @@
+export const stakingContractAddress = "0xB0880D6f2Cc4eDE914BB73474f945cb428e8215A";
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { RskTestnet } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={RskTestnet}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
